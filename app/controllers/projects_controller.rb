6f888643421eca392b75bc0d79ab2bad5173ea02
Gloriane <glorianetran@gmail.com>
2018-03-05 09:17:23 +0000

    route working

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,11 @@
+class ProjectsController < ApplicationController
+  def index
+    
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'projects/' => redirect('projects/index')
+  
+  get 'projects/index'
+
+  get 'projects/new'
+
+  get 'projects/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'static_pages/home'
   get 'static_pages/about'
