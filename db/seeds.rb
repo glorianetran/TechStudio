8e30aee945e9ce7e66270e611553964df1546bab
Gloriane <glorianetran@gmail.com>
2018-03-05 08:38:44 +0000

    entered seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tech_studio = Project.create! title: "Tech Studio", creator: "Team Make It Work", description: "First attempt at succeeding."
+tech_studio = Project.create!(:title => 'Tech Studio', :creator => 'Team Make It Work',
+    :description => 'Platform for collaboration on interdisciplinary tech projects.')
+arduino = Project.create!(:title => 'Arduino Knitwear Project', :creator => 'Suzie McStrugglebus',
+    :description => 'I want to create fun knitted hats with lights and music.')
+music = Project.create!(:title => 'Computer Music Band', :creator => 'Logarhythms',
+    :description => "Let's make nerdy computer music together!")
+
+tech_studio.tag_list=("Rails, web")
+arduino.tag_list=("C, Arduino, knitting, wearables")
+music.tag_list=("music, Ableton, web")
 
