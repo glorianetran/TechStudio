fe6ff9e408ed7b27e71c9a8f164fe64bf412faa8
Gloriane <glorianetran@gmail.com>
2018-03-05 09:28:43 +0000

    projects page working with mock data

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :projects
+  
   get 'projects/' => redirect('projects/index')
   
-  get 'projects/index'
+  # get 'projects/index'
 
-  get 'projects/new'
+  # get 'projects/new'
 
-  get 'projects/show'
+  # get 'projects/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/home'
