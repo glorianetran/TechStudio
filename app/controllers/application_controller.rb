09e5bfb57b3ab83de28246d94f783ff93b6a5695
Gloriane <glorianetran@gmail.com>
2018-03-27 04:23:33 +0000

    login on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
  protect_from_forgery with: :exception
   helper_method :current_user
 
-  def authenticate
-  	redirect_to :login unless user_signed_in?
-  end
-
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
