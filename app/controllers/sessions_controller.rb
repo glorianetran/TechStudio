21eab6688b20037461efcef9f09633fe93811eb6
Gloriane <glorianetran@gmail.com>
2018-03-27 04:09:50 +0000

    authenticated projects page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path
+  	@user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
+  	session[:user_id] = @user.id
+  	redirect_to root_path
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+  	session[:user_id] = nil
+  	redirect_to root_path
   end
+
 end
