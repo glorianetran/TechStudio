19e6fd4f066c0dae537caff1569dc242ccddeb7e
Gloriane <glorianetran@gmail.com>
2018-04-03 05:12:41 +0000

    fixed session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def create
   	@user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
   	session[:user_id] = @user.id
-  	redirect_to root_path
+  	redirect_to "/"
   end
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to root_path
+  	redirect_to "/"
   end
 
 end
