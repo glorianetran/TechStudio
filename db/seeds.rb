26875e748e9c3aaec2136e92ea24eebfc97d1742
Gloriane <glorianetran@gmail.com>
2018-04-05 18:14:09 +0000

    more test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 tech_studio = Project.create!(:title => 'Tech Studio', :summary => 'Platform for collaboration on interdisciplinary tech projects.',:description => 'This is the description', :project_type => 'Hobby', :skills_desired => 'ruby')
-#painting = Project.create!(:title => 'Tech Art', :summary => 'Art and tech is life.',:description => 'This is the description', :project_type => 'Hobby', :skills_desired => 'painting, cs')
+painting = Project.create!(:title => 'Tech Art', :summary => 'Art and tech is life.', :description => 'This is the description', :project_type => 'Hobby', :skills_desired => 'painting, cs')
 music = Project.create!(:title => 'Computer Music Band', :summary => "Let's make nerdy computer music together!", :project_type => "Hobby")
 
 
 tech_studio.tag_list=("Rails, web, Bootstrap, front end, databases")
 music.tag_list=("music, Ableton, web")
-#painting.tag_list=('painting, lights, tech')
+painting.tag_list=('painting, lights, tech')
 
 # add collaborators
 user1 = User.create!(:name => 'Regina')
@@ -21,16 +21,20 @@ user2 = User.create!(:name => 'Glo')
 user3 = User.create!(:name => 'Deana')
 user4 = User.create!(:name => 'Natasha')
 
+# creator 
+tech_studio.add_creator=user2.id
+painting.add_creator=user1.id
+music.add_creator = user4.id
+
+# collaborator
+
 tech_studio.add_collaborator=user1.id
 
 music.add_collaborator=user3.id
 music.add_collaborator=user1.id
 
-# painting.add_collaborator = user2.id
-# painting.add_collaborator = user3.id
-# painting.add_collaborator = user4.id
+painting.add_collaborator = user2.id
+painting.add_collaborator = user3.id
+painting.add_collaborator = user4.id
 
 # add a creator
-tech_studio.add_creator=user2.id
-music.add_creator=user4.id
-#painting.add_creator=user1.id
