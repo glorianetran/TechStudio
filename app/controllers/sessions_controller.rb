66fdfbcbbbfa3fd7299ea47ece6d1f92df78286e
Regina Wang <reginawang@live.com>
2018-04-10 17:38:25 +0000

    sessions_controller.rb edited online with Bitbucket

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-  # skip_before_action :store_return_to
   def create
   	@user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
   	session[:user_id] = @user.id
@@ -10,10 +9,6 @@ class SessionsController < ApplicationController
     redirect_to(session[:return_to] || default)
     session[:return_to] = nil
   end
-  
-  # def store_return_to
-  #   session[:return_to] = request.url
-  # end
 
   def destroy
   	session[:user_id] = nil
