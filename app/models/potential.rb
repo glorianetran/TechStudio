99c76f68243ab39ece9efc1402ff2f19f21ccff3
Regina Wang <reginawang@live.com>
2018-04-10 02:03:40 +0000

    Implemented getting user's projects

--- app/models/potential.rb
+++
@@ -1,4 +0,0 @@
-class Potential < ApplicationRecord
-  belongs_to :user
-  belongs_to :project
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_many :projectusers
-    has_many :potential_projects, ->{where(projectusers: {collaborator: true})}, through: :projectusers
-    has_many :collab_projects, ->{ where(projectusers: {collaborator: true})}, through: :projectusers
+    has_many :potential_projects, ->{where(projectusers: {collaborator: true})}, through: :projectusers, :class_name=> 'Project', :source => :project
+    has_many :collab_projects, ->{ where(projectusers: {collaborator: true})}, through: :projectusers, :class_name=> 'Project', :source => :project
 	def self.find_or_create_from_auth_hash(auth)
 		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
           user.provider = auth.provider
@@ -11,5 +11,9 @@ class User < ActiveRecord::Base
           user.oauth_expires_at = Time.at(auth.credentials.expires_at)
           user.save!
 		end
+	end
+	
+	def created_projects
+	    Project.find_by(id: Projectuser.select('project_id').where(user_id: self.id).where(creator:true))
 	end
 end
