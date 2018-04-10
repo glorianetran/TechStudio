fcd78354fa83548bdaabf29ed10e3e7619ef669b
Gloriane <glorianetran@gmail.com>
2018-04-10 17:36:36 +0000

    deleted unused code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :store_return_to
+  # skip_before_action :store_return_to
   def create
   	@user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
   	session[:user_id] = @user.id
@@ -11,9 +11,9 @@ class SessionsController < ApplicationController
     session[:return_to] = nil
   end
   
-  def store_return_to
-    session[:return_to] = request.url
-  end
+  # def store_return_to
+  #   session[:return_to] = request.url
+  # end
 
   def destroy
   	session[:user_id] = nil
