19d3bb35a13b3420b61424a9158ef5f5ee1e6b8b
Deana <dbui@mills.edu>
2018-04-12 17:35:02 +0000

    change to front page

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,8 @@ ActiveRecord::Schema.define(version: 20180405011807) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.string "summary"
+    t.string "creator"
     t.text "description"
-    t.string "project_type"
-    t.string "skills_desired"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
