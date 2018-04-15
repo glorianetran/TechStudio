f139f94b177c85b2257be3f84ce2ee9975aed67c
Deana <dbui@mills.edu>
2018-04-15 00:21:12 +0000

    changes to create project

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 20180405011807) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.string "creator"
+    t.string "summary"
     t.text "description"
+    t.string "project_type"
+    t.string "skills_desired"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
